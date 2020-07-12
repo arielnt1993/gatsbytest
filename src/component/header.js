39b27933f43e1173731b97d47b2ad5b6a436a3eb
Ariel <a.t.navarro.1993@gmail.com>
2020-07-12 04:50:33 -0300

    using scss

--- src/component/header.js
+++ src/component/header.js
@@ -1,19 +1,30 @@
 import React from "react"
 import { Link } from "gatsby"
+
+import "./header.scss"
+
 const Header = () => {
   return (
     <div>
       <p>my site</p>
       <nav>
-      <ul>
-        <li><Link to="/">home</Link></li>
-        <li><Link to="/blog">blog</Link></li>
-        <li><Link to="/about">about</Link></li>
-        <li><Link to="/contact">contact</Link></li>
-      </ul>
+        <ul>
+          <li>
+            <Link to="/">home</Link>
+          </li>
+          <li>
+            <Link to="/blog">blog</Link>
+          </li>
+          <li>
+            <Link to="/about">about</Link>
+          </li>
+          <li>
+            <Link to="/contact">contact</Link>
+          </li>
+        </ul>
       </nav>
     </div>
   )
 }
 
-export default Header;
+export default Header
